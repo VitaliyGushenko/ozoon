aaf8c2d5257c8ef1980b14a87e66b127473cc8e6
DESKTOP-GEDM3JE\Vitaliy <gushenko759@gmai.com>
2026-09-18 21:14:52 +0300

    Connect Firebase (@angular/fire)

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDGXMLop_XDnOaY6bmgqcUPgtB6uxN8g8A',
+    authDomain: 'marketplace-ec4bb.firebaseapp.com',
+    projectId: 'marketplace-ec4bb',
+    storageBucket: 'marketplace-ec4bb.firebasestorage.app',
+    messagingSenderId: '202132503126',
+    appId: '1:202132503126:web:bf83caa39cdb8f4e9299f7',
+  },
+};
